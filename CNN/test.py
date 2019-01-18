843546b5d83c01a48f78375d6c0aef2ac75176a5
Yonghan Kim <nearkyh@naver.com>
2019-01-18 14:31:46 +0900

    mnist, cifar10 데이터셋을 사용한 CNN 구현

---
+++ CNN/test.py
@@ -0,0 +1,49 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from utils.datasets import mnist
+from utils.datasets import cifar10
+
+from keras.models import load_model
+
+
+# mnist_get = mnist.pre_processing()
+# num_classes = mnist_get['num_classes']
+# x_train = mnist_get['x_train']
+# x_test = mnist_get['x_test']
+# y_train = mnist_get['y_train']
+# y_test = mnist_get['y_test']
+# input_shape = mnist_get['input_shape']
+
+cifar10_get = cifar10.pre_processing()
+num_classes = cifar10_get['num_classes']
+x_train = cifar10_get['x_train']
+x_test = cifar10_get['x_test']
+y_train = cifar10_get['y_train']
+y_test = cifar10_get['y_test']
+input_shape = cifar10_get['input_shape']
+
+# 학습된 모델 로드
+# model = load_model('utils/save_models/cnn_mnist.h5')
+model = load_model('utils/save_models/cnn_cifar10.h5')
+
+# 모델 테스트
+test_data_len = 3
+test_data_index = np.random.choice(x_test.shape[0], test_data_len) # shuffle 및 데이터 선택
+test_data = x_test[test_data_index]
+predict_data = model.predict_classes(test_data)
+
+
+
+if __name__ == '__main__':
+
+    for i in range(test_data_len):
+        (_x_train, _y_train), (_x_test, _y_test) = cifar10.load_data()
+        plt.figure()
+        plt.imshow(_x_test[test_data_index[i]])
+        plt.colorbar()
+        plt.grid(False)
+        plt.show()
+
+        print('Input : ' + str(np.argmax(y_test[test_data_index[i]]))
+              + ', Predict : ' + str(predict_data[i]))
---
+++ CNN/train.py
@@ -0,0 +1,91 @@
+from __future__ import print_function
+
+import os
+import argparse
+
+from utils.datasets import mnist
+from utils.datasets import cifar10
+from utils.models.cnn import ConvolutionNeuralNetwork
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--dataset', default='mnist', type=str,
+                    help="mnist, cifar10")
+parser.add_argument('--batch_size', default=32, type=int,
+                    help="Number of batch_size")
+parser.add_argument('--epochs', default=1, type=int,
+                    help="Number of epochs")
+parser.add_argument('--gpu', default=False, type=bool,
+                    help="Using GPU")
+args = parser.parse_args()
+batch_size = args.batch_size
+epochs = args.epochs
+
+
+
+if __name__ == '__main__':
+
+    if args.dataset == 'mnist':
+        mnist_get = mnist.pre_processing()
+        num_classes = mnist_get['num_classes']
+        x_train = mnist_get['x_train']
+        x_test = mnist_get['x_test']
+        y_train = mnist_get['y_train']
+        y_test = mnist_get['y_test']
+        input_shape = mnist_get['input_shape']
+
+        cnn = ConvolutionNeuralNetwork(input_shape=input_shape,
+                                       num_classes=num_classes,
+                                       gpu=args.gpu)
+        model = cnn.mnist_model()
+
+        # 모델 학습
+        model.fit(x_train, y_train,
+                  batch_size=batch_size,
+                  epochs=epochs,
+                  verbose=1,
+                  validation_data=(x_test, y_test))
+
+        # 모델 평가
+        score = model.evaluate(x_test, y_test, verbose=0)
+        print('Test loss:', score[0])
+        print('Test accuracy:', score[1])
+
+        # 학습 모델 저장
+        try:
+            if not (os.path.isdir('utils/save_models')):
+                os.makedirs(os.path.join('utils/save_models'))
+        except Exception as e:
+            print("Failed to create directory!!!")
+        model.save('utils/save_models/cnn_mnist.h5')
+
+    elif args.dataset == 'cifar10':
+        cifar10_get = cifar10.pre_processing()
+        num_classes = cifar10_get['num_classes']
+        x_train = cifar10_get['x_train']
+        x_test = cifar10_get['x_test']
+        y_train = cifar10_get['y_train']
+        y_test = cifar10_get['y_test']
+        input_shape = cifar10_get['input_shape']
+
+        cnn = ConvolutionNeuralNetwork(input_shape=input_shape,
+                                       num_classes=num_classes,
+                                       gpu=args.gpu)
+        model = cnn.cifar10_model()
+
+        model.fit(x_train, y_train,
+                  batch_size=batch_size,
+                  epochs=epochs,
+                  verbose=1,
+                  validation_data=(x_test, y_test))
+
+        score = model.evaluate(x_test, y_test, verbose=0)
+        print('Test loss:', score[0])
+        print('Test accuracy:', score[1])
+
+        try:
+            if not(os.path.isdir('utils/save_models')):
+                os.makedirs(os.path.join('utils/save_models'))
+        except Exception as e:
+            print("Failed to create directory!!!")
+        model.save('utils/save_models/cnn_cifar10.h5')
